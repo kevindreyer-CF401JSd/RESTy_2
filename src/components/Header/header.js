2ef07cb95774c24843c8cd1aa5ffec56051eca13
AstroKD <kdreyerinseattle@gmail.com>
2020-03-19 22:53:51 -0700

    built from a local create react app and then copied into a repo

---
+++ src/components/Header/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../../24278.svg';
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>
+        RESTy
+      </h1>
+    </header>
+  )
+}
+
+export default Header
